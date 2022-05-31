1629fdd8e9ea67288a2bffba03b0bcfc5ef50436
Samuel Emard-Thibault <92962712+SamEThibault@users.noreply.github.com>
2022-05-31 13:45:07 -0400

    added languages stack

--- portfolio/src/views/Home.jsx
+++ portfolio/src/views/Home.jsx
@@ -17,16 +17,25 @@ function Home() {
             </div>
 
             <img className="hand-gif" src="https://c.tenor.com/nebZyl8oN7IAAAAi/wave-hello.gif" width="80" />
-           
-            <div className="intro-container">
 
+            <div className="intro-container">
                 <p className="intro-text">
-                I'm a Computer Engineering Student at Queen's University in Ontario, Canada. When I need a break from my studies, I enjoy working on web applications and exploring the constant flow of emerging frameworks / libraries. 
-                Feel free to reach out via my LinkedIn profile <a className="link" href="https://www.linkedin.com/in/samuelemardthibault/" > right here!</a>
+                    I'm a Computer Engineering Student at Queen's University in Ontario, Canada. When I need a break from my studies, I enjoy working on web applications and exploring the constant flow of emerging frameworks / libraries.
+                    Feel free to reach out via my LinkedIn profile <a className="link" href="https://www.linkedin.com/in/samuelemardthibault/" > right here!</a>
                 </p>
+
+                <h1 className="intro-text" >Tech Stack</h1>
+                <h2 className="languages"> Languages: </h2>
+
+                <div className="icons-container-languages">
+                    <img className="icons" width="130" src="https://img.shields.io/badge/-Java-black?style=flat-square&logo=java"></img>
+                    <img className="icons" width="212" src="https://img.shields.io/badge/-JavaScript-black?style=flat-square&logo=javascript"></img>
+                    <img className="icons" width="170" src="https://img.shields.io/badge/-C/C++-black?style=flat-square&logo=c"></img>
+                    <img className="icons" width="170" src="https://img.shields.io/badge/-Python-black?style=flat-square&logo=Python"></img>
+                </div>
             </div>
 
-  
+
         </div>
     );
 }
